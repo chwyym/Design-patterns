45381c168e03301c40476a7bbe4b5f53c47b30e1
User <User@DESKTOP-JB01J8L>
2019-11-04 22:14:46 +0900

    20191104 FactoryPatterns Code

--- src/factory/AbstractFactory/store/NYPizzaStore.java
+++ src/factory/AbstractFactory/store/NYPizzaStore.java
@@ -10,12 +10,15 @@ public class NYPizzaStore extends PizzaStore{
 	@Override
 	public Pizza createPizza(String type) {
 		Pizza pizza = null;
+		
 		//뉴욕 피자 성분 팩토리
 		PizzaIngredientFactory ingredientFactory = 
 				new NyPizzaIngredintFactory();
 		
+		
 		if(type.equals("cheese")) {
 			pizza = new CheesePizza(ingredientFactory);
+			pizza.setName("뉴욕 치즈피자");
 		}
 		
 		return pizza;
