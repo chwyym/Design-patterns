dde7d76c3b7881c1da9a50861fae30615ccd84ca
User <User@DESKTOP-JB01J8L>
2019-11-17 17:12:52 +0900

    20191117 Iterator&Composite pattern Code

---
+++ src/composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package composite.menu;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
